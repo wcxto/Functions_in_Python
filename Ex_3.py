7164e98443aa33cd542ddf022c39367d1bc6f43c
wcxto <163146366+wcxto@users.noreply.github.com>
2024-08-15 15:14:19 +0300

    Create Ex_3.py

---
+++ Ex_3.py
@@ -0,0 +1,19 @@
+# Значение многочлена
+
+3 Многочленом степени n называется выражение вида
+
+# Напишите функцию evaluate(coefficients, x), которая принимает список коэффициентов и значение аргумента x. 
+# При реализации можно использовать анонимные lambda-функции, а также встроенные функции map() и reduce().
+
+# Способ 1:
+
+from functools import reduce
+import operator
+def evaluate(a, x):
+	xi = map(lambda i: x**i, range(len(a)-1, -1, -1))
+	axi = map(operator.mul, a, xi)         	
+	return reduce(operator.add, axi, 0)
+a = list(map(int, input().split()))
+x = int(input())
+print(evaluate(a, x))
+
